79f9538350ee3eca794fb6ca4e5923266f2722e4
Mihail Davidevski <majkl_david@hotmail.com>
2023-04-12 20:55:54 +0200

    add body parser, add static files

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 const express = require("express");
-
+const path = require("path");
+const bodyParser = require("body-parser");
 const port = 3000;
 
 const app = express();
 
+app.use(bodyParser.urlencoded());
+
+app.set();
+
+app.use(express.static(path.join(__dirname, "public")));
+
 app.get("/", (req, res) => {
-  res.send("<h1>Hello from express</h1>");
+  res.sendFile(path.join(__dirname, "views", "index.html"));
+});
+
+app.post("/products", (req, res) => {
+  const prod = req.body;
+  console.log(prod);
+  res.sendFile(path.join(__dirname, "views", "products.html"));
+});
+app.get("/add-product", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "add-product.html"));
 });
 
 app.listen(port, () => {
